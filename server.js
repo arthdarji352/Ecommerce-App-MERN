b6142027f6ce8129bd3af05bac4f85de1a55b065
arthdarji352 <arthdarji20@gmail.com>
2023-07-28 12:31:33 +0530

    server

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from "path";
 //confige env
 dotenv.config();
 
@@ -19,16 +20,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 //rest api
-app.get("/", (req, res) => {
-  res.send({
-    message: "welcome to ecom app",
-  });
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 //PORT
